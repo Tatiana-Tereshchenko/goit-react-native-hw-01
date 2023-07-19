5c5db14e04519d120d0deb72205dda47ed7aac04
Tatiana-Tereshchenko <teresenkot33@gmail.com>
2023-07-19 22:49:32 +0300

    +

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -10,11 +11,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
